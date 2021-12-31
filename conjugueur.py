d90a03d4ac12d61c2711a2f7b2b6606091192e3e
doddyz <doddyz@live.com>
2021-12-31 11:43:46 +0000

    Pre All Tables reindexing after findall filter refinement

--- conjugueur.py
+++ conjugueur.py
@@ -17,18 +17,12 @@ def get_page_soup(verb):
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
 
-# def get_page_elts(verb):
-#     page = get_page(verb)
-#     soup = BeautifulSoup(page, 'html.parser')
-    
-    # get_page_mode_tables(verb)
-    # get_page_tense_tables(verb, mode)
-    # get_page_tense_forms(verb, mode)
-    
 
 def get_page_mode_tables(verb):
     soup = get_page_soup(verb)
-    all_tables = soup.find_all('table', class_=is_none_css)
+    # all_tables = soup.find_all('table', width='100%')
+    all_tables = soup.find_all(border='0', width='100%')
+    print(all_tables)
     return_dict = {}
     return_dict['modes'] = all_tables[0]
     return_dict['verbe_du_n_groupe'] = all_tables[1]
@@ -43,10 +37,9 @@ def get_page_mode_tables(verb):
 def get_mode_tense_sub_tables(verb, mode):
     if (mode == 'indicatif'):
         mode_table = get_page_mode_tables(verb)['indicatif']
-        # print(mode_table)
+
     sub_tables = mode_table.find_all('table')
 
-    # print(sub_tables)
     
     sub_tables_dict = {
         'present': sub_tables[0],
@@ -86,29 +79,20 @@ def df_from_mode_tense_sub_table(verb, mode, tense, is_compound_tense):
         pronoums_and_verb_forms = [elt[0].strip() + ' ' + elt[1] for elt in list(zip(pronoums_and_auxiliary, participles))]
 
     return pd.DataFrame({mode_tense_sub_table_header: pronoums_and_verb_forms})
-        
-    # print(test, pronoums, verb_forms)
-    
-    # Get verb forms 
-    
-    
-    # Works only for non compound tenses like present, imparfait, passe simple
-    # pronoums_tds = mode_tense_sub_table.find_all(align='right')
-    # pronoums = [pronoums_tds[i].text.strip() for i,_ in enumerate(pronoums_tds) if i % 2 == 0]
-    # verb_forms_tds = mode_tense_sub_table.find_all(align='left')
-    # verb_forms = [verb_forms_tds[i].text.strip() for i,_ in enumerate(verb_forms_tds) if i % 2 == 0]
-    # pronoums_and_verb_forms = [elt[0] + ' ' + elt[1] for elt in list(zip(pronoums, verb_forms))]
+            
 
-    # return pd.DataFrame({mode_tense_sub_table_header: pronoums_and_verb_forms})
-    
-# To check whether an element has a set class
+
+# Findall filter functions 
+def has_width_attr(tag):
+    return tag.has_attr('width')
+
 def is_none_css(css_class):
     return css_class is None
 
 
     
 # Play
-df_from_mode_tense_sub_table('aimer', 'indicatif', 'present')
+# df_from_mode_tense_sub_table('aimer', 'indicatif', 'present')
 # df_from_mode_tense_sub_table('aimer', 'indicatif', 'passe_compose')
     
 
